98490043a15a808f63e916a151d7d95c3bfc63be
Devon <cococodeco@gmail.com>
2016-06-13 15:52:07 +0800

    make a listview to discover smart lids

--- app/src/main/java/com/domain/thermosandroid/TestActivity.java
+++ app/src/main/java/com/domain/thermosandroid/TestActivity.java
@@ -1,40 +1,68 @@
 package com.domain.thermosandroid;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
-import com.idevicesinc.sweetblue.BleDevice;
-import com.idevicesinc.sweetblue.BleDeviceState;
 import com.idevicesinc.sweetblue.BleManager;
 import com.idevicesinc.sweetblue.BleManagerConfig;
 import com.idevicesinc.sweetblue.utils.BluetoothEnabler;
-import com.idevicesinc.sweetblue.utils.Uuids;
+
+import java.util.ArrayList;
 
 public class TestActivity extends AppCompatActivity {
 
     private static final String TAG = "TestActivity";
-    TextView scanText;
-    String scanString="scan";
+    String scanString = "scan";
+    public BleManager m_bleManager;
+    SharedPreferences prefs;
+    ListView mListView;
+    ArrayList<String> mList;
+    ArrayAdapter<String> arrayAdapter;
+    ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        scanText = (TextView) findViewById(R.id.textView2);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        prefs = this.getSharedPreferences("ThermosWITS", Context.MODE_PRIVATE);
+        mList = new ArrayList<String>();
+        mList.clear();
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
     }
 
     @Override
     protected void onStart() {
-// A ScanFilter decides whether a BleDevice instance will be created from a
-// BLE advertisement and passed to the DiscoveryListener implementation below.
+        mProgressBar.setVisibility(View.VISIBLE);
+        // A ScanFilter decides whether a BleDevice instance will be created from a
+        // BLE advertisement and passed to the DiscoveryListener implementation below.
         final BleManagerConfig.ScanFilter scanFilter = new BleManagerConfig.ScanFilter() {
-            @Override public Please onEvent(BleManagerConfig.ScanFilter.ScanEvent e) {
+            @Override
+            public Please onEvent(BleManagerConfig.ScanFilter.ScanEvent e) {
                 Log.i(TAG, "1111111111111111" + e);
-                scanString = scanString + e + "\n\n\n\n\n";
-                scanText.setText(scanString);
-                return BleManagerConfig.ScanFilter.Please.acknowledgeIf(e.name_normalized().contains("thermos"))
-                        .thenStopScan();
+                return BleManagerConfig.ScanFilter.Please.acknowledgeIf(e.name_normalized().contains("thermos"));
+//                        .thenStopScan();
+
+                //thermos_34C7
+                //thermos_053B
             }
         };
 
@@ -44,51 +72,74 @@ public class TestActivity extends AppCompatActivity {
         // Obviously you will want to structure your actual code a little better.
         // The deep nesting simply demonstrates the async-callback-based nature of the API.
         final BleManager.DiscoveryListener discoveryListener = new BleManager.DiscoveryListener() {
-            @Override public void onEvent(DiscoveryEvent e) {
-                Log.i(TAG, "onEvent A =  " + e);
-                scanString = scanString + "discover: " + e +" \n\n\n\n";
-                scanText.setText(scanString);
+            @Override
+            public void onEvent(DiscoveryEvent e) {
+//                Log.i(TAG, "onEvent A =  " + e);
+                Log.i(TAG, "onEvent Aname =  " + e.device().getName_debug());
 
-                if( e.was(BleManager.DiscoveryListener.LifeCycle.DISCOVERED) ) {
-                    e.device().connect(new BleDevice.StateListener() {
-                        @Override
-                        public void onEvent(StateEvent e) {
-                            Log.i(TAG, "connect =  " + e);
-                            scanString = scanString + "connect: " + e +" \n\n\n\n";
-                            scanText.setText(scanString);
+                mList.add(e.device().getName_debug());
 
-                            if( e.didEnter(BleDeviceState.INITIALIZED) ) {
-                                e.device().read(Uuids.BATTERY_LEVEL, new BleDevice.ReadWriteListener() {
-                                    @Override
-                                    public void onEvent(ReadWriteEvent e) {
-                                        if(e.wasSuccess()){
-                                            Log.i("", "Battery level is " + e.data_byte() + "%");
-                                            scanString = scanString + "readbattery state: " + e.data_byte() + "%" +" \n";
-                                            scanText.setText(scanString);
-                                        }
-                                    }
-                                });
-                            }
-                        }
-                    });
-                }
+//              if( e.was(BleManager.DiscoveryListener.LifeCycle.DISCOVERED) ) {
+//                  e.device().connect(new BleDevice.StateListener() {
+//                      @Override
+//                      public void onEvent(StateEvent e) {
+//                          Log.i(TAG, "connect =  " + e);
+//                          scanString = scanString + "connect: " + e +" \n\n\n\n";
+//                          scanText.setText(scanString);
+//
+//                          if( e.didEnter(BleDeviceState.INITIALIZED) ) {
+//                              e.device().read(Uuids.BATTERY_LEVEL, new BleDevice.ReadWriteListener() {
+//                                  @Override
+//                                  public void onEvent(ReadWriteEvent e) {
+//                                      if(e.wasSuccess()){
+//                                          Log.i("", "Battery level is " + e.data_byte() + "%");
+//                                          scanString = scanString + "readbattery state: " + e.data_byte() + "%" +" \n";
+//                                          scanText.setText(scanString);
+//                                      }
+//                                  }
+//                              });
+//                          }
+//                      }
+//                  });
+//              }
             }
         };
 
-// This class helps you navigate the treacherous waters of Android M Location requirements for scanning.
-// First it enables bluetooth itself, then location permissions, then location services. The latter two
-// are only needed in Android M.
+        // This class helps you navigate the treacherous waters of Android M Location requirements for scanning.
+        // First it enables bluetooth itself, then location permissions, then location services. The latter two
+        // are only needed in Android M.
         BluetoothEnabler.start(this, new BluetoothEnabler.DefaultBluetoothEnablerFilter() {
-            @Override public Please onEvent(BluetoothEnabler.BluetoothEnablerFilter.BluetoothEnablerEvent e) {
-                if( e.isDone() ) {
+            @Override
+            public Please onEvent(BluetoothEnabler.BluetoothEnablerFilter.BluetoothEnablerEvent e) {
+                if (e.isDone()) {
                     e.bleManager().startScan(scanFilter, discoveryListener);
+                    m_bleManager = e.bleManager();
                 }
+
+                //stop at 10 seconds
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        m_bleManager.stopScan();
+                        arrayAdapter = new ArrayAdapter<String>
+                                (TestActivity.this, android.R.layout.simple_list_item_1 , mList);
+                        for (int i=0; i<mList.size(); i++)
+                            Log.v(TAG, "check mList is " +mList.get(i));
+                        mListView.setAdapter(arrayAdapter);
+                        mProgressBar.setVisibility(View.GONE);
+                    }
+                }, 5000);
+
                 return super.onEvent(e);
             }
         });
 
+
+
         super.onStart();
     }
+
+
 
     @Override
     protected void onResume() {
